1b709a667dab0a79c769ddc692c52afa6042ccf5
nguyen long <longnguyenhoang26@gmail.com>
2020-11-19 17:51:26 +0700

    commit

---
+++ QL_Blogs/QL_Blogs/Blog.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace QL_Blogs
+{
+    public class Blog
+    {
+
+        private void GetUserDetails()
+        {
+            var json = File.ReadAllText(@"F:\DigiDinos\Blog_DigiDinos\json_server_blog\db.json");
+            try
+            {
+                var jObject = JObject.Parse(json);
+
+                if (jObject != null)
+                {
+                    Console.WriteLine("ID :" + jObject["id"].ToString());
+                    Console.WriteLine("Title :" + jObject["title"].ToString());
+                    Console.WriteLine("Des :" + jObject["des"].ToString());
+                    Console.WriteLine("Detail :" + jObject["detail"].ToString());
+                    Console.WriteLine("Category :" + jObject["category"].ToString());
+                    Console.WriteLine("Public :" + jObject["public"].ToString());
+                    Console.WriteLine("Data_pubblic :" + jObject["data_pubblic"].ToString());
+                    Console.WriteLine("Position :" + jObject["position"].ToString());
+                    Console.WriteLine("Thumbs :" + jObject["thumbs"].ToString());
+                    JArray experiencesArrary = (JArray)jObject["experiences"];
+
+                }
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ QL_Blogs/QL_Blogs/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QL_Blogs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
